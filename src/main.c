0a7e98253e0e26cf271fb830f69d9d49365d5522
ZigZag35 <rayenpilote0@gmail.com>
2022-05-01 19:55:25 +0000

    update

--- src/main.c
+++ src/main.c
@@ -4,19 +4,25 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <sys/stat.h>
 
-static void die(char *message);
+static char *version = "1.0.0";
+
+static void die(char *message, ...);
 static void debug(char *message);
 static void usage(void);
 static void create_directory_tree(char *, char*);
-static void create_makefile(char *project, char *type, char *compiler);
-static void create_main(char *project, char *type);
+static void create_makefile(char *project, char *extension, char *compiler);
+static void create_main(char *project, char *extension);
 static void create_readme(char *project);
 static void init_git(char *project);
 
-void die(char *message){
-    fprintf(stderr, "[!!] %s\n", message);
+void die(char *message, ...){
+    va_list ap;
+    va_start(ap, message);
+    vfprintf(stderr, message, ap);
+    va_end(ap);
     exit(1);
 }
 
@@ -25,11 +31,11 @@ void debug(char *message){
 }
 
 void usage(void){
-    printf("USAGE: yapm <name> <type> -c <compiler>\n");
+    printf("USAGE: yapm <name> -e <extension> -c <compiler>\n");
     exit(1);
 }
 
-void create_directory_tree(char *project, char *type){
+void create_directory_tree(char *project, char *extension){
     char buffer[256];
     if (mkdir(project, DIR_PERM) != 0)
         die("creating directory.");
@@ -44,7 +50,7 @@ void create_directory_tree(char *project, char *type){
     memset(buffer, 0, sizeof(buffer));
 }
 
-void create_makefile(char *project, char *type, char *compiler){
+void create_makefile(char *project, char *extension, char *compiler){
     char buffer[256];
     sprintf(buffer, "%s/Makefile", project);
     FILE *makefile = fopen(buffer, "w");
@@ -59,25 +65,25 @@ void create_makefile(char *project, char *type, char *compiler){
 		    "	mv bin/%s ${PREFIX}/bin/ \n"
 		    "\n"
 		    "clean:\n"
-		    "	rm -f bin/%s\n", project, type, compiler, project, project, project);
+		    "	rm -f bin/%s\n", project, extension, compiler, project, project, project);
     fprintf(makefile, "%s", buffer);
     fclose(makefile);
 }
 
-void create_main(char *project, char *type){
+void create_main(char *project, char *extension){
     char buffer[256];
-    sprintf(buffer, "%s/src/main.%s", project, type);
+    sprintf(buffer, "%s/src/main.%s", project, extension);
     FILE *main = fopen(buffer, "w");
     if (main == NULL)
         die("creating main file.");
-    if (strcmp(type, "c") == 0){
+    if (strcmp(extension, "c") == 0){
         sprintf(buffer, "#include <stdio.h> \n"
                         " \n"
                         "int main(int argc, char **argv) { \n"
                         "	printf(\"Hello, world!\"); \n"
                         "	return 0; \n"
                         "} \n");
-    } else if (strcmp(type, "cpp") == 0 ) {
+    } else if (strcmp(extension, "cpp") == 0 ) {
         sprintf(buffer, "#include <iostream> \n"
                         " \n"
                         "int main(int argc, char *argv[]) { \n"
@@ -107,27 +113,38 @@ void init_git(char *project){
 }
 
 int main(int argc, char **argv) {
-    if (argc < 3)
+    if (argc < 2)
         usage();
     int opt;
     char buffer[256];
     char project[80];
-    char type[8];
+    char extension[8];
     char compiler[16];
     strncpy(project, argv[1], 80);
-    strncpy(type, argv[2], 8);
+    strncpy(extension, "c", 8);
     strcpy(compiler, "gcc");
-    while ((opt = getopt(argc, argv, ":c:")) != -1) {
+    while ((opt = getopt(argc, argv, ":hvt:c:")) != -1) {
         switch (opt) {
-            case 'c':
-                strncpy(compiler, optarg, 16);
-                break;
+        case 'h':
+            usage();
+            break;
+        case 'v':
+            die("yapm v%s", version);
+            break;
+        case 't':
+            strncpy(extension, optarg, 8);
+            break;
+        case 'c':
+            strncpy(compiler, optarg, 16);
+            break;
+        default:
+            die("unknown option: %s", opt);
         }
     }
 
-    create_directory_tree(project, type);
-    create_makefile(project, type, compiler);
-    create_main(project, type);
+    create_directory_tree(project, extension);
+    create_makefile(project, extension, compiler);
+    create_main(project, extension);
     create_readme(project);
     // init_git(project);
 
